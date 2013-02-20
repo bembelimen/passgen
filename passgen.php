9a565b29a669cb628523a5287d0f91ccfee95813
Helrunar <info@projektwaterkant.de>
2013-02-21 00:15:33 +0100

    set trigger and beginn with JS

--- passgen.php
+++ passgen.php
@@ -6,5 +6,47 @@ jimport('joomla.event.plugin');
 
 class plgUserPassgen extends JPlugin
 {
+    /**
+     * Constructor
+     *
+     * @access      protected
+     * @param       object  $subject The object to observe
+     * @param       array   $config  An array that holds the plugin configuration
+     * @since       1.5
+     */
+    public function __construct(& $subject, $config)
+    {
+        parent::__construct($subject, $config);
+        $this->loadLanguage();
+    }
 
+    /**
+     * @param	string	$context	The context for the data
+     * @param	int		$data		The user id
+     * @param	object
+     *
+     * @return	boolean
+     * @since	1.6
+     */
+    function onContentPrepareData($context, $data)
+    {
+        // Check we are manipulating a valid form.
+        if (!in_array($context, array('com_users.profile', 'com_users.user', 'com_users.registration', 'com_admin.profile')))
+        {
+            return true;
+        }
+
+
+        $document = & JFactory::getDocument();
+        $document->addScript("http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
+        $document->addScript("http://jquery-easypassgen.googlecode.com/svn/trunk/jquery.easypassgen.js");
+        $document->addCustomTag( '<script type="text/javascript">jQuery.noConflict();</script>' );
+        $document->addCustomTag( '<script type="text/javascript">jQuery(document).ready(function(){
+
+$("div.password").easypassgen();
+
+});</script>' );
+        echo '<div class="password">Test 2</div>';
+
+    }
 }
