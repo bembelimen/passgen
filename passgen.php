4777c3b46b061e31c8d993a086adbb4398fd169b
Helrunar <info@projektwaterkant.de>
2013-02-21 13:45:38 +0100

    ready to work as V1.0

--- passgen.php
+++ passgen.php
@@ -21,18 +21,17 @@ class plgUserPassgen extends JPlugin
     }
 
     /**
-     * @param	string	$context	The context for the data
-     * @param	int		$data		The user id
-     * @param	object
+     * @param    string    $context    The context for the data
+     * @param    int        $data        The user id
+     * @param    object
      *
-     * @return	boolean
-     * @since	1.6
+     * @return    boolean
+     * @since    1.6
      */
     function onContentPrepareData($context, $data)
     {
         // Check we are manipulating a valid form.
-        if (!in_array($context, array('com_users.profile', 'com_users.user', 'com_users.registration', 'com_admin.profile')))
-        {
+        if (!in_array($context, array('com_users.profile', 'com_users.user', 'com_users.registration', 'com_admin.profile'))) {
             return true;
         }
 
@@ -40,13 +39,14 @@ class plgUserPassgen extends JPlugin
         $document = & JFactory::getDocument();
         $document->addScript("http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
         $document->addScript("http://jquery-easypassgen.googlecode.com/svn/trunk/jquery.easypassgen.js");
-        $document->addCustomTag( '<script type="text/javascript">jQuery.noConflict();</script>' );
-        $document->addCustomTag( '<script type="text/javascript">jQuery(document).ready(function(){
+        $document->addCustomTag('<script type="text/javascript">jQuery.noConflict();</script>');
+        $document->addCustomTag('<script type="text/javascript">jQuery(document).ready(function($){
+        $("div.password").easypassgen();
+        });</script>');
 
-$("div.password").easypassgen();
 
-});</script>' );
-        echo '<div class="password">Test 2</div>';
+        $outputpass = '<div id="passgen">' .JTEXT::_("PLG_USER_PASSGEN_PROPOSITION"). '<div class="password"></div></div>';
+        print $outputpass;
 
     }
 }
